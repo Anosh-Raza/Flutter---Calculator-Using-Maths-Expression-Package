df3c3cf5a625dffd22b4668548b95ea2124d17b3
Anosh-Raza <anoshraxa@gmail.com>
2022-10-04 09:51:36 +0500

    Calculator Using Math Expression

---
+++ lib/constant.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const Color kGrayColor = Colors.grey;
+const Color kOrangeColor = Colors.lightBlue;
+const styleText = TextStyle(fontSize: 16, color: Colors.white);
